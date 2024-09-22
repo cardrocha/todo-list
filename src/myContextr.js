4c03203ba38c51aa30a37ccd873fb897995fc111
cardrocha <cardrocharj@gmail.com>
2024-09-22 10:03:20 -0300

    feat: contextAPI

---
+++ src/myContextr.js
@@ -0,0 +1,18 @@
+'use client'
+
+import { createContext, useState, useContext } from "react";
+
+
+const ModalContext = createContext();
+
+export const ModalProvider = ({ children }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <ModalContext.Provider value={{ isModalOpen, setIsModalOpen }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModal = () => useContext(ModalContext);
